d82eb694ea09b36ad7c45cd5fbb6a0d5a9fedc9b
tobe-snapnet <tobe@snapnet.com.ng>
2022-03-15 20:45:25 +0100

    create api auth

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/register',[UserController::class,'register']);
+Route::post('/login',[UserController::class,'login']);
+Route::group(['middleware'=>['auth:sanctum']],function(){
+    Route::post('/logout',[UserController::class,'logout']);
+   
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
